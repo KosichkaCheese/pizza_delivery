650b9a8c33445f18218dd7778e00b6a7b4382336
KosichkaCheese <selivanovaalina9@gmail.com>
2024-10-17 21:53:10 +0500

    idk what happened

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # import uvicorn
 from fastapi import FastAPI
 from fastapi import APIRouter
-import settings
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+# import settings
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.orm import sessionmaker
 from users import user_router
 
-engine = create_async_engine(settings.DATABASE_URL)
-session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+# engine = create_async_engine(settings.DATABASE_URL)
+# session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 app = FastAPI(title='Pizza API', version='0.0.1')
 
